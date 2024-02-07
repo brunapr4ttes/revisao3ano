912db91b8d9b35cf9a7f9de2f68ae8fd79b74db4
bruna <brunapratesrobotica@gmail.com>
2024-02-07 12:37:02 -0300

    Modificações e atualizações -> OBJECTS, TESTE FRONTEND, PROJETOREVISAO.

--- Documents/KAWANE3E/TESTE_DE_FRONTEND/script.js
+++ Documents/KAWANE3E/TESTE_DE_FRONTEND/script.js
@@ -1,27 +1,27 @@
-let peso, altura
+function calcular(){
+const peso = Number(document.getElementById("peso").value)
+const altura = Number(document.getElementById("altura").value)
+let imc = peso / (altura * altura)
 
-peso = window.document.getElementById(peso).value
-altura = window.document.getElementById(altura).value
-let imc = (peso / altura) * 2
+if (imc <= 16.9) {
+    document.getElementById("resultado").innerHTML = `${imc.toFixed(2)} - Muito abaixo do peso`
 
-if (imc < 16.9) {
-    document.write(`Muito abaixo do peso`)
-
-}else if(imc >= 17 && imc <= 18.5){
-    document.write(`Abaixo do peso`)
+}else if(imc >= 17 && imc <= 18.4){
+    document.getElementById("resultado").innerHTML = `${imc.toFixed(2)} - Abaixo do peso`
 
 }else if(imc >= 18.5 && imc <= 24.9){
-    document.write(`Peso normal`)
+    document.getElementById("resultado").innerHTML = `${imc.toFixed(2)} - Peso normal`
 
 }else if(imc >= 25 && imc <= 29.9){
-    document.write(`Acima do peso`)
-
+    document.getElementById("resultado").innerHTML = `${imc.toFixed(2)} - Acima do peso`
+    
 }else if(imc >= 30 && imc <= 34.9){
-    document.write(`Obesidade grau I`)
+    document.getElementById("resultado").innerHTML = `${imc.toFixed(2)} - Obesidade grau I`
 
 }else if(imc >= 35 && imc <= 40){
-    document.write(`Obesidade grau II`)
+    document.getElementById("resultado").innerHTML = `${imc.toFixed(2)} - Obesidade grau II`
 
 }else if(imc > 40){
-    document.write(`Obesidade grau III`)
+    document.getElementById("resultado").innerHTML = `${imc.toFixed(2)} - Obesidade grau III`
+    }
 }
